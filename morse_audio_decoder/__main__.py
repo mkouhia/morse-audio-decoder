6a8527d246f42725319396f2df20feae07992ddf
Mikko Kouhia <mikko.kouhia@iki.fi>
2022-02-28 22:02:22 +0200

    Reorder main, to get poetry script working
    As a script, main is called without arguments. Therefore parsing
    of argv must happen within.

--- morse_audio_decoder/__main__.py
+++ morse_audio_decoder/__main__.py
@@ -1,31 +1,38 @@
 """Command line interface"""
 
 import argparse
-import os
 from pathlib import Path
 import sys
 
 from morse_audio_decoder.morse import MorseCode
 
 
-def main(file: os.PathLike) -> None:
+def main(argv: list[str] = None) -> None:
     """Read WAV file, process it and write outputs to stdout
 
     Args:
-        file (os.PathLike): path to WAV file
+        argv (list[str]): List of command line arguments. Defaults to None.
 
     Raises:
         UserWarning: If dash/dot separation cannot be made unambiguosly,
             or if input file does not exist.
     """
+    parsed_args = _parse_args(argv)
+    file = parsed_args.WAVFILE
+
     if not Path(file).exists():
-        raise UserWarning(f"File {file} not found, exiting.")
+        sys.stderr.write(f"File {file} not found, exiting.\n")
+        sys.exit(1)
 
-    decoded = MorseCode.from_wavfile(file).decode()
-    sys.stdout.write(decoded + "\n")
+    try:
+        decoded = MorseCode.from_wavfile(file).decode()
+        sys.stdout.write(decoded + "\n")
+    except UserWarning as err:
+        sys.stderr.write(f"{err}\n")
+        sys.exit(1)
 
 
-def parse_args(args: list[str]) -> argparse.Namespace:
+def _parse_args(args: list[str]) -> argparse.Namespace:
     """Parse arguments from command line"""
     parser = argparse.ArgumentParser(
         description="""Read audio file in WAV format, extract the morse code and
@@ -36,9 +43,4 @@ def parse_args(args: list[str]) -> argparse.Namespace:
 
 
 if __name__ == "__main__":
-    program_arguments = parse_args(sys.argv[1:])
-    try:
-        main(program_arguments.WAVFILE)
-    except UserWarning as err:
-        sys.stderr.write(f"{err}\n")
-        sys.exit(1)
+    main()
--- tests/test_morse_audio_decoder.py
+++ tests/test_morse_audio_decoder.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 
 from morse_audio_decoder import __version__
-from morse_audio_decoder.__main__ import main, parse_args
+from morse_audio_decoder.__main__ import main, _parse_args
 
 
 def test_version():
@@ -38,20 +38,48 @@ def test_main(mocker, tmp_path: Path, capsys):
 
     mocker.patch("morse_audio_decoder.__main__.MorseCode", TestClass)
 
-    main(file_path)
+    main([str(file_path)])
     captured = capsys.readouterr()
     assert captured.out == expected
 
 
+def test_main_raises(mocker, tmp_path: Path, capsys):
+    """UserWarning causes exit with error message"""
+    file_path = tmp_path / "any_file"
+    file_path.touch()
+
+    err_msg = "test error message"
+
+    # pylint: disable=all
+    class TestClass:
+        def __init__(self, data):
+            self.data = data
+
+        def decode(self):
+            raise UserWarning(err_msg)
+
+        @classmethod
+        def from_wavfile(cls, file):
+            return cls(np.array([0, 1]))
+
+    mocker.patch("morse_audio_decoder.__main__.MorseCode", TestClass)
+
+    with pytest.raises(SystemExit):
+        main([str(file_path)])
+
+    captured = capsys.readouterr()
+    assert captured.err == err_msg + "\n"
+
+
 def test_main_not_existing():
     """When file is not existing, raise warning"""
-    with pytest.raises(UserWarning):
-        main("not-existing-filename")
+    with pytest.raises(SystemExit):
+        main(["not-existing-filename"])
 
 
 def test_parser_wavfile():
     """Argument parser receives filename"""
     test_filename = "test_file"
-    parser = parse_args([test_filename])
+    parser = _parse_args([test_filename])
 
     assert parser.WAVFILE == test_filename
